ff709c5ed99c029db275d48026360eaf81b9e9a7
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-06-24 14:37:00 +0530

    Fetch JSON from API and Decode

---
+++ Shared/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
